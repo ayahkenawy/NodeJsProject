394b15a3ba3e361f5c9b9b46e1f874725a32bd66
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2022-03-01 23:56:36 +0200

    Angular Register & Login

--- AngularNodeJs/src/app/pages/user/register/register.component.ts
+++ AngularNodeJs/src/app/pages/user/register/register.component.ts
@@ -8,15 +8,28 @@ import { AuthService } from 'src/app/providers/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private _auth:AuthService) { 
+  msg: string = ""
+  flag:boolean=false
+  constructor(private _auth: AuthService) {
 
   }
 
   ngOnInit(): void {
   }
-  handleRegister(registerForm:NgForm){
-// console.log(registerForm.value)
-this._auth.register(registerForm.value).subscribe(data=>console.log(data))
+  handleRegister(registerForm: NgForm) {
+    // console.log(registerForm.value)
+    if (registerForm.valid) {
+      this._auth.register(registerForm.value).subscribe(data => {
+        console.log(data)
+        if (data.apiStatus) {
+          this.msg = "Data Added Successfuly"
+          this.flag=true
+        }
+        else { this.msg = "Error Adding Data" }
+      }
+      )
+
+    }
+
   }
 }
