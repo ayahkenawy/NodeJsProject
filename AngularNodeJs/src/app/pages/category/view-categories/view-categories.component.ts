1debef4f0f67b5e42c1db508b8086f58c112acb6
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2022-03-04 19:43:21 +0200

    edit

--- AngularNodeJs/src/app/pages/category/view-categories/view-categories.component.ts
+++ AngularNodeJs/src/app/pages/category/view-categories/view-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/providers/services/auth.service';
 
 @Component({
   selector: 'app-view-categories',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-categories.component.css']
 })
 export class ViewCategoriesComponent implements OnInit {
-
-  constructor() { }
+cats:any=[]
+  constructor(private _auth:AuthService) { }
 
   ngOnInit(): void {
+    this.getAllCats()
   }
+getAllCats(){
+  this._auth.getAllCategories().subscribe((res)=>{
+    this.cats=res
+    console.log(res)
+  },
+  (e)=>{
 
+  }
+  ,()=>{
+
+  })
+}
 }
